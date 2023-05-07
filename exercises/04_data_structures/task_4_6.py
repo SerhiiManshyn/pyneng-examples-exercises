d52983d2249e99d1ca39ea706e6c377bb9eef89e
SerhiiManshyn <andokaj@gmail.com>
2023-05-07 23:34:55 +0300

    4.6 completed

--- exercises/04_data_structures/task_4_6.py
+++ exercises/04_data_structures/task_4_6.py
@@ -20,11 +20,17 @@ ospf_route. Значения из строки ospf_route надо получи�
 проверять результат.
 """
 
-ospf_route = "      10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
+ospf_route = "      10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0" 
+ospf_route = ospf_route.split()
+ospf_route[1] = ospf_route[1].strip('[]')
+ospf_route[3] = ospf_route[3].strip(',')
+ospf_route[4] = ospf_route[4].strip(',')
 template = """
-Prefix                {}
-AD/Metric             {}
-Next-Hop              {}
-Last update           {}
-Outbound Interface    {}
+Prefix                {0}
+AD/Metric             {1}
+Next-Hop              {2}
+Last update           {3}
+Outbound Interface    {4}
 """
+print(template.format(ospf_route[0], ospf_route[1], ospf_route[3],ospf_route[4], ospf_route[5]))
+
